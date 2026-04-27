cf85f4f60ce4a3291e137d214f036929efecc517
Benjamin Krug <benjaminkrug@yahoo.com>
2026-04-27 14:44:05 -0400

    Add an endpoint that tests the DB connection

--- DragonPlacementApi/Program.cs
+++ DragonPlacementApi/Program.cs
@@ -1,7 +1,16 @@
+using DragonPlacementDataLayer.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddEntityFrameworkSqlite()
+    .AddDbContext<DragonPlacementContext>((options) =>
+        options.UseSqlite("Data Source=../Database/DragonPlacement.db"));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Dragon Placement API!");
-app.MapGet("/dragons", () => "Return a list of dragons");
+app.MapGet("/dragons", (DragonPlacementContext context) => context.Dragons.Take(20).ToList());
 
 app.Run();
