1094d79ab1c9f94ae5c9098676116abf1d32fc2e
Benjamin Krug <benjaminkrug@yahoo.com>
2026-04-28 16:45:48 -0400

    Add allowed origins

--- DragonPlacementApi/Program.cs
+++ DragonPlacementApi/Program.cs
@@ -3,7 +3,17 @@ using DragonPlacementDataLayer.Models;
 using DragonPlacementDataLayer.Repositories;
 using Microsoft.EntityFrameworkCore;
 
+const string allowedOriginsPolicy = "DragonPlacementAllowedOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
+
+var allowedOrigins = builder.Configuration.GetValue<string>("AllowedOrigins")?.Split(",") ?? [];
+builder.Services.AddCors(options =>
+    options.AddPolicy(
+        allowedOriginsPolicy,
+        policy => policy.WithOrigins(allowedOrigins).AllowCredentials().AllowAnyHeader().AllowAnyMethod()
+    )
+);
 
 builder.Services
     .AddEntityFrameworkSqlite()
@@ -14,6 +24,7 @@ builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepositor
 builder.Services.AddScoped<IAssignmentUnitOfWork, AssignmentUnitOfWork>();
 
 var app = builder.Build();
+app.UseCors(allowedOriginsPolicy);
 
 app.MapGet("/", () => "Dragon Placement API!");
 app.MapGet("/dragon", DragonEndpoints.GetDragons);
--- DragonPlacementDataLayer/ModelExtensions/Assignment.cs
+++ DragonPlacementDataLayer/ModelExtensions/Assignment.cs
@@ -2,12 +2,14 @@ namespace DragonPlacementDataLayer.Models;
 
 public partial class Assignment
 {
+    public DateTime StartDate => DateTimeOffset.FromUnixTimeSeconds(StartDateUnix).UtcDateTime;
     public DateTime GetStartDate() => DateTimeOffset.FromUnixTimeSeconds(StartDateUnix).UtcDateTime;
     public void SetStartDate(DateTime value)
     {
         StartDateUnix = new DateTimeOffset(value).ToUnixTimeSeconds();
     }
 
+    public DateTime? EndDate => EndDateUnix == null ? null : DateTimeOffset.FromUnixTimeSeconds(EndDateUnix.Value).UtcDateTime;
     public DateTime? GetEndDate() => EndDateUnix == null ? null : DateTimeOffset.FromUnixTimeSeconds(EndDateUnix.Value).UtcDateTime;
     public void SetEndDate(DateTime? value)
     {
