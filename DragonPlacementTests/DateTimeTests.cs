f9dec0ecf1ef20f289276a53e838f4091c162085
Benjamin Krug <benjaminkrug@yahoo.com>
2026-05-14 11:28:20 -0400

    Refactor a test

--- DragonPlacementTests/DateTimeTests.cs
+++ DragonPlacementTests/DateTimeTests.cs
@@ -7,61 +7,35 @@ namespace DragonPlacementTests;
 /// These tests make certain that our code can avoid the 2038 problem.
 /// If Unix epoc times are stored as 32-bit signed integers, some dates will be outside of the range of what is possible.
 /// If an iteration of this test ever fails, we probably are representing a date as "int" instead of "long".
-/// SUGGESTION: If I ever make time to rewrite this test, use reflection instead of delegates. The failure message will be more meaningful.
 /// </summary>
 public class DateTimeTests
 {
-    delegate DateTime SetAndGetDate(DateTime targetDate);
-
-    private DateTime AssignmentStartDate(DateTime targetDate)
+    private static DateTime RoundTripStartDate<T>(DateTime targetDate) where T : new()
     {
-        var assignment = new Assignment();
-        assignment.SetStartDate(targetDate);
-        return assignment.GetStartDate();
+        var obj = new T();
+        typeof(T).GetMethod("SetStartDate")!.Invoke(obj, [targetDate]);
+        return (DateTime)typeof(T).GetMethod("GetStartDate")!.Invoke(obj, null)!;
     }
 
-    private DateTime AssignmentEndDate(DateTime targetDate)
+    private static DateTime RoundTripEndDate<T>(DateTime targetDate) where T : new()
     {
-        var assignment = new Assignment();
-        assignment.SetEndDate(targetDate);
-        return assignment.GetEndDate();
+        var obj = new T();
+        typeof(T).GetMethod("SetEndDate")!.Invoke(obj, [targetDate]);
+        return (DateTime)typeof(T).GetMethod("GetEndDate")!.Invoke(obj, null)!;
     }
-
-    private DateTime JobStartDate(DateTime targetDate)
-    {
-        var assignment = new Job();
-        assignment.SetStartDate(targetDate);
-        return assignment.GetStartDate();
-    }
-
-    private DateTime JobEndDate(DateTime targetDate)
-    {
-        var assignment = new Job();
-        assignment.SetEndDate(targetDate);
-        return assignment.GetEndDate();
-    }    
 
     [Theory]
     [InlineData(1901, 2, 1)]     //This date would not be possible with 32-bit signed integer
     [InlineData(1969, 12, 15)]
     [InlineData(1970, 1, 15)]
     [InlineData(2038, 12, 25)]   //This date would not be possible with 32-bit signed integer
-    [InlineData(2106, 3, 5)]     //This date would not be possible with 32-bit unsigned integer  
+    [InlineData(2106, 3, 5)]     //This date would not be possible with 32-bit unsigned integer
     public void Assignment_SetStartDate_ReturnSameDate(int year, int month, int date)
     {
         var targetDate = new DateTime(year, month, date, 0, 0, 0, DateTimeKind.Utc);
-        var delegates = new List<SetAndGetDate>()
-        {
-            AssignmentStartDate,
-            AssignmentEndDate,
-            JobStartDate,
-            JobEndDate
-        };
-        for (int i = 0; i < delegates.Count; ++i)
-        {
-            var testDelagate = delegates[i];
-            var calculatedDate = testDelagate(targetDate);
-            calculatedDate.ShouldBe(targetDate, TimeSpan.FromSeconds(1), $"Failure happend running delegate at index {i}.");
-        }
+        RoundTripStartDate<Assignment>(targetDate).ShouldBe(targetDate, TimeSpan.FromSeconds(1));
+        RoundTripEndDate<Assignment>(targetDate).ShouldBe(targetDate, TimeSpan.FromSeconds(1));
+        RoundTripStartDate<Job>(targetDate).ShouldBe(targetDate, TimeSpan.FromSeconds(1));
+        RoundTripEndDate<Job>(targetDate).ShouldBe(targetDate, TimeSpan.FromSeconds(1));
     }
 }
