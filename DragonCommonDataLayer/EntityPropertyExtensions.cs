c950de048d2685b218b566e2c28001ca344b3b40
Benjamin Krug <benjaminkrug@yahoo.com>
2026-06-25 18:28:31 -0400

    Add documentation

--- DragonCommonDataLayer/EntityPropertyExtensions.cs
+++ DragonCommonDataLayer/EntityPropertyExtensions.cs
@@ -9,10 +9,14 @@ public static class EntityPropertyExtensions
     /// Used for a domain model whose field is of type DateTime,
     /// but an SQLite database stores the value as a long integer.
     /// </summary>
-    public static void IsUnixSecondsType(this PropertyBuilder<DateTime> dateTimeProp, string fieldName)
+    /// <param name="databaseFieldName">
+    /// The name of the field as it appears in the database.
+    /// Often the same as the domain's field name, but with the "Unix" suffix added.
+    /// </param>
+    public static void IsUnixSecondsType(this PropertyBuilder<DateTime> domainModelProp, string databaseFieldName)
     {
-        dateTimeProp
-            .HasColumnName(fieldName)
+        domainModelProp
+            .HasColumnName(databaseFieldName)
             .HasConversion(
                 d => new DateTimeOffset(d, TimeSpan.Zero).ToUnixTimeSeconds(),
                 unix => DateTimeOffset.FromUnixTimeSeconds(unix).UtcDateTime
