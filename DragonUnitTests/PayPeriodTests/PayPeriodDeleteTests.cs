3f68211e57d0a7aaa046a6161b0368c7434f682d
Benjamin Krug <benjaminkrug@yahoo.com>
2026-08-21 21:36:53 -0400

    Assert that pay period in "billed" status cannot be deleted.

--- DragonUnitTests/PayPeriodTests/PayPeriodDeleteTests.cs
+++ DragonUnitTests/PayPeriodTests/PayPeriodDeleteTests.cs
@@ -5,16 +5,24 @@ using Microsoft.AspNetCore.Http.HttpResults;
 using Moq;
 using Shouldly;
 using DragonTimekeeping.Application;
+using DragonTimekeeping.Domain.Enums;
 
 namespace DragonUnitTests.PayPeriodTests;
 
 public class PayPeriodDeleteTests
 {
-    [Fact]
-    public async Task DeletePayPeriod_Exists_ExpectOkAndSavesOnce()
+    [Theory]
+    [InlineData(PayPeriodStatus.Draft)]
+    [InlineData(PayPeriodStatus.Submitted)]
+    public async Task DeletePayPeriod_AcceptableStatus_ExpectOkAndSavesOnce(PayPeriodStatus existingStatus)
     {
         const int PAY_PERIOD_ID = 42;
+        var payPeriod = new PayPeriodBuilder()
+            .WithPayPeriodId(PAY_PERIOD_ID)
+            .WithSubmissionStatus(existingStatus)
+            .Build();
         var unitOfWorkMock = new Mock<ITimekeepingUnitOfWork>();
+        unitOfWorkMock.Setup(u => u.PayPeriodRepository.GetByID(PAY_PERIOD_ID)).ReturnsAsync(payPeriod);
         unitOfWorkMock.Setup(u => u.PayPeriodRepository.Delete(PAY_PERIOD_ID)).Returns(DeleteResult.Deleted);
 
         //Act
@@ -23,6 +31,26 @@ public class PayPeriodDeleteTests
         //Assert
         response.Result.ShouldBeOfType<Ok<ValidatedResponse>>();
         unitOfWorkMock.Verify(u => u.SaveAsync(), Times.Once);
+    }
+
+    [Fact]
+    public async Task DeletePayPeriod_BilledStatus_ExpectConflictAndDoesNotSave()
+    {
+        const int PAY_PERIOD_ID = 43;
+        var payPeriod = new PayPeriodBuilder()
+            .WithPayPeriodId(PAY_PERIOD_ID)
+            .WithSubmissionStatus(PayPeriodStatus.Billed)
+            .Build();
+        var unitOfWorkMock = new Mock<ITimekeepingUnitOfWork>();
+        unitOfWorkMock.Setup(u => u.PayPeriodRepository.GetByID(PAY_PERIOD_ID)).ReturnsAsync(payPeriod);
+
+        //Act
+        var response = await PayPeriodEndpoints.DeletePayPeriodAsync(unitOfWorkMock.Object, PAY_PERIOD_ID);
+
+        //Assert
+        response.Result.ShouldBeOfType<Conflict<ValidatedResponse>>();
+        unitOfWorkMock.Verify(u => u.PayPeriodRepository.Delete(It.IsAny<int>()), Times.Never);
+        unitOfWorkMock.Verify(u => u.SaveAsync(), Times.Never);
     }
 
     [Fact]
